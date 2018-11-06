21e9a2e58a91b17444a3c17e3f5281a6434e1af7
Laura Gonzalez <lauragonaguirre@gmail.com>
2018-11-06 15:02:01 -0600

    added progress bar

--- src/app.js
+++ src/app.js
@@ -8,3 +8,24 @@ AOS.init({
     easing: 'ease-in-out-sine',
     delay: 100,
 });
+
+function fillingBar() {
+    var elem = document.getElementById("mybar");
+    var current_progress = 0;
+    var interval = setInterval(frame, 300);
+    
+    function frame() {
+        current_progress++;
+        elem.style.width = current_progress + '%'; 
+        if (current_progress >= 100) {
+            clearInterval(interval);
+        }
+    }
+}
+
+function progressBar() {
+    document.getElementById("paymentButton").addEventListener("click", fillingBar);
+};
+
+progressBar();
+
